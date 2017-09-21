a5eb6a5e2ebbbb50d4dc32214816ff833c2c0f60
Rosa Ekström <rjosih@hotmail.com>
2017-09-21 15:25:32 +0200

    confused

--- Member.cs
+++ Member.cs
@@ -1,5 +1,6 @@
 using System;
 
+
 namespace W2
 {
 	class Member
@@ -9,55 +10,76 @@ namespace W2
 			Console.WriteLine("Write 'delete', 'change', 'create' or 'show' members?");
 			string answer = Console.ReadLine();
 			
-			if(answer == "delete")
-			{
-				//show members
-				Console.WriteLine("Write which member you wanna delete");
-				string deleteMember = Console.ReadLine();
+			// if(answer == "delete")
+			// {
+			// 	//show members
+			// 	Console.WriteLine("Write which member you wanna delete");
+			// 	string deleteMember = Console.ReadLine();
 
-				if(deleteMember == MemberInList)
-				{	
-					//radera medlemmen
-					Console.WriteLine("The member has been removed");
-				}
-			}
-			else if(answer == "change")
-			{
-				//show members
-				Console.WriteLine("Write which member you wanna change");
-				string changeMember = Console.ReadLine();
-				if(changeMember == MemberInList)
-				{
-					//ändra den
-					//spara den
-				}
-			}
-			else if(answer == "create")
+			// 	if(deleteMember == memberInList)
+			// 	{	
+			// 		//radera medlemmen
+			// 		Console.WriteLine("The member has been removed");
+			// 	}
+			// }
+			// else if(answer == "change")
+			// {
+			// 	Console.WriteLine(memberInList);
+			// 	Console.WriteLine("Write which member you wanna change");
+			// 	string changeMember = Console.ReadLine();
+			// 	if(changeMember == memberInList)
+			// 	{
+			// 		//ändra den
+			// 		//spara den
+			// 	}
+			// }
+			if(answer == "create")
 			{
 				Console.WriteLine("Name");
 				string name = Console.ReadLine();
+				
+				Console.WriteLine("Personal number");
+				long personalNumber = Convert.ToInt64(Console.ReadLine());
+				
 				Console.WriteLine("Member ID");
-				string memberID = Console.ReadLine();
+				int memberID =Convert.ToInt32(Console.ReadLine());
+				
 				Console.WriteLine("Number of boats");
-				string numberOfBoats = Console.ReadLine();
+				int numberOfBoats = Convert.ToInt32(Console.ReadLine());
+				
 				Console.WriteLine("Boat details");
 				string boatDetails = Console.ReadLine();
-				Console.WriteLine("Personal number");
 				
+				//spara till memberInformation.txt
+				Console.WriteLine("The member has been saved!");
 			}
-			else if(answer == "show")
-			{
-				//show list
-				Console.WriteLine(MembersList);
-				if(MembersList)
-				{
-
-				}
-			}
+			// else if(answer == "show")
+			// {
+			// 	//show list ex, for loop som går igenom alla 
+			// 	Console.WriteLine("Do you want the members in a 'compact' list or 'verbose' list?");
+			// 	string listAnswer = Console.ReadLine();
+			// 	if(listAnswer == "compact")
+			// 	{
+			// 		Console.WriteLine(compactList);
+			// 	}
+			// 	else if(listAnswer == "verbose")
+			// 	{
+			// 		Console.WriteLine(verboseList);
+			// 	}
+			// 	else
+			// 	{
+			// 		Console.WriteLine("You need to answer compact or verbose");
+			// 	}
+				
+			// }
 			else
 			{
 				Console.WriteLine("You need to answer change, delete, create or show");
 			}
+		 }
+		 public class Create
+		 {
+			 
 		 }
 	}
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace W2
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("What do you want to do? Choose number \n 1. Create a member \n 2. See Compact List\n 3. See Verbose List \n 4. Delete a member \n 5. Change a member's information \n ");
 		}
 	}
 }
