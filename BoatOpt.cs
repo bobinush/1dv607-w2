6efb741279f9feb47427d15f5f0a5b4861d50a2a
Frida Melin <fm222gn@student.lnu.se>
2017-09-25 13:42:32 +0200

    ändrade om lite kring input och tänkte börja på att ta bort en båt men programmet går inte att testa nu så väntar lite med det

--- BoatOpt.cs
+++ BoatOpt.cs
@@ -8,39 +8,35 @@ namespace W2
 	{
 		public void Boats(string[] args)
 		{
-
-			//START
-			Console.WriteLine("Delete | Change | Create |");
+			Console.WriteLine("1 Delete | 2 Change | 3 Create |");
 			string choices = Console.ReadLine();
-
+			double inputChoice = Int32.Parse(choices);
 
 			//DELETE
-			if (choices == "Delete")
+			if (inputChoice == 1)
 			{
-
-				Console.WriteLine("Wich boat do you want to delete?");
-				//visa båtar BoatInformation()
+				Console.WriteLine("Wich members boat do you want to delete?");
+				//visa en lista över medlemmarna 
 			}
 
 			//CHANGE
-			else if (choices == "Change")
+			else if (inputChoice == 2)
 			{
 				Console.WriteLine("Wich boat do you want to change?");
 				//visa båtar BoatInformation()
 			}
 
 			//CREATE
-			else if (choices == "Create")
+			else if (inputChoice == 3)
 			{
 				Console.WriteLine("Choose type:");
 				Console.WriteLine("1 Sailboat | 2 Motorsailer | 3 Kayak/Canoe | 4 Other|");
 				string type = Console.ReadLine();
 				int inputType = Int32.Parse(type);
 
-
 				Console.WriteLine("Enter the boats length (meters)");
 				string length = Console.ReadLine();
-				double inputLenght = Int32.Parse(length);
+				int inputLenght = Int32.Parse(length);
 
 				var boat = new Boat(inputType, inputLenght);
 				//spara till databas
@@ -49,7 +45,7 @@ namespace W2
 			}
 			else
 			{
-				Console.WriteLine("You need to answer Delete, Change & Create");
+				Console.WriteLine("You need to answer 1 Delete, 2 Change & 3 Create");
 			}
 
 
