e732a35a7b09fada3373e39a9a2458ef7fad03c5
Robin Nowakowski <bobinush@gmail.com>
2017-09-27 15:38:11 +0200

    call membersopt from program

--- Program.cs
+++ Program.cs
@@ -15,20 +15,8 @@ namespace W2
 			// 	{
 			var db = new Database();
 			db.LoadDatabase();
-			var member = new Member("Robin", "910524-xxxx");
-			var boat = new Boat((int)Enums.BoatTypes.KayakCanoe, 14.1);
-			member.AddBoat(boat);
-			member.AddBoat(boat);
-			db.Save(member);
-			member.Name = "kalle anka";
-			db.Save(member);
-			var newBoat = new Boat(2, 1.3);
-			member.UpdateBoat(boat, newBoat);
-			db.Save(member);
-			var membersList = db.GetAllMembers();
-			membersList.ForEach(x => Console.WriteLine(x.ToVerbose()));
-			member.DeleteBoat(boat.Id);
-			membersList.ForEach(x => Console.WriteLine(x.ToVerbose()));
+			var members = new MemberOpt();
+			members.Main(db);
 
 			// 	}
 			// 	catch (Exception ex)
