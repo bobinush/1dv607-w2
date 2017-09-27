29944c3cfedb3fb0916715a682f7692b4624aeeb
Rosa Ekström <rjosih@hotmail.com>
2017-09-27 15:21:04 +0200

    member structure

--- MemberOpt.cs
+++ MemberOpt.cs
@@ -6,35 +6,12 @@ namespace W2
 {
 	class MemberOpt
 	{
-		 public void Main(string[] args)
+		 public void DoMemberList(string[] args)
 		 { 
-			Console.WriteLine("Write 'delete', 'change', 'create' or 'show'");
+			Console.WriteLine("1 Create | 2 Show");
 			string answer = Console.ReadLine();
-			
-			// if(answer == "delete")
-			// {
-			// 	//show members
-			// 	Console.WriteLine("Write which member you wanna delete");
-			// 	string deleteMember = Console.ReadLine();
 
-			// 	if(deleteMember == memberInList)
-			// 	{	
-			// 		//radera medlemmen
-			// 		Console.WriteLine("The member has been removed");
-			// 	}
-			// }
-			// else if(answer == "change")
-			// {
-			// 	Console.WriteLine(memberInList);
-			// 	Console.WriteLine("Write which member you wanna change");
-			// 	string changeMember = Console.ReadLine();
-			// 	if(changeMember == memberInList)
-			// 	{
-			// 		//ändra den
-			// 		//spara den
-			// 	}
-			// }
-			if(answer == "create")
+			if(answer == "1")
 			{
 				Console.WriteLine("Name");
 				string name = Console.ReadLine();
@@ -52,29 +29,54 @@ namespace W2
 				Console.WriteLine("The member has been saved!");
 				// string text = System.IO.File.ReadAllText(@"C:\wamp64\www\1dv610\1dv607-w2\memberInformation.txt");
 				
-			}			
-			// else if(answer == "show")
-			// {
-			// 	//show list ex, for loop som går igenom alla 
-			// 	Console.WriteLine("Do you want the members in a 'compact' list or 'verbose' list?");
-			// 	string listAnswer = Console.ReadLine();
-			// 	if(listAnswer == "compact")
-			// 	{
-			// 		Console.WriteLine(compactList);
-			// 	}
-			// 	else if(listAnswer == "verbose")
-			// 	{
-			// 		Console.WriteLine(verboseList);
-			// 	}
-			// 	else
-			// 	{
-			// 		Console.WriteLine("You need to answer compact or verbose");
-			// 	}
+			}		
+					else if(answer == "2")
+			{
+				// visar alla members + member id
+				Console.WriteLine("Which member? ");
+				int findMemberId = Int32.Parse(Console.ReadLine());
+				Member chosenMember = Member.member.Find(x => x.Id == id);
+
+				Console.WriteLine("1 Edit | 2 Delete | 3 View Boat");
+				int memberChoises = Int32.Parse(Console.ReadLine());
+
+				if(memberChoises == "1")
+			{
+				Console.WriteLine("Name");
+				string nameEdit = Console.ReadLine();
 				
-			// }
+				Console.WriteLine("Personal number");
+				string personalNumberEdit = Console.ReadLine();
+
+				//save
+				Member.Save();
+			}
+				else if(memberChoises == "2")
+				{
+					//show members
+					Console.WriteLine("Write which memberID you wanna delete");
+					string deleteMember = Console.ReadLine();
+
+					if(deleteMember = Member.member.Find(x => x.Id == id))
+					{	
+						Console.WriteLine("Are you sure? Yes/no");
+						
+						string deleteOpt = Console.ReadLine();
+						if(deleteOpt == "yes")
+						{
+							//deletea
+							Member.Delete();
+							// gå tillbaka till show memberlist
+							Show.MemberList();
+						}
+					}
+				}
+	
+	
+			}
 			else
 			{
-				Console.WriteLine("You need to answer change, delete, create or show");
+				Console.WriteLine("You need to answer '1' create or '2' show");
 			}
 		 }
 		 public class Create
